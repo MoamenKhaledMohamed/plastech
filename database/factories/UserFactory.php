25853a10aebe25eea3f06b4f96ed7f7f812b7dd6
MoamenKhaledMohamed <60798424+MoamenKhaledMohamed@users.noreply.github.com>
2021-01-31 15:56:00 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,8 +23,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'firstname' => $this->faker->firstNameFemale,
-            'lastname' => $this->faker->lastName,
+            'first_name' => $this->faker->firstNameFemale,
+            'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
